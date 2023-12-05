61076afa997aed35da1a67fb8fea423fd98e0376
antpao2 <152311053+antpao2@users.noreply.github.com>
2023-12-05 11:50:31 -0500

    Add files via upload

--- Project_S.R
+++ Project_S.R
@@ -4,12 +4,12 @@ library(nflfastR)
 library(gsisdecoder)
 library(qs)
 library(fuzzyjoin)
-
+library(dplyr)
 
 #Loading Player Stats by Week
 all <- load_player_stats(2003:2022)
 wr <- all %>% filter(position == 'WR', season_type == 'REG')
-wr <- wr %>% select(names(wr)[c(1,3,4,7,8,35:49)] )
+wr <- wr %>% dplyr::select(names(wr)[c(1,3,4,7,8,35:49)] )
 #Go over why some player names won't pull
 #wr is per week
 
@@ -28,7 +28,6 @@ p$name <- ifelse(p$player_id == '00-0027702', 'Mike Williams2', p$name )
 
 #Name and rookie szn (Not sure if needed)
 o <- p %>% group_by(name) %>% summarise(l = min(season))
-
 
 
 #Draft Pick Data
@@ -57,22 +56,102 @@ draft_wr$pfr_name <- str_replace(draft_wr$pfr_name, 'Michael Pittman Jr.','Micha
 all <- draft_wr %>% left_join(p, by = c('pfr_name'= 'name', 'season' = 'season'))
 #All are all rookie stats with their draft position
 
-
 #Going to omit the ones that didnt match or didnt record a catch in their rookie season 
 ###May not have matched bc of name, position different, etc, or may be blank bc no stats (injured, etc.)
+### Will have to explain this in our paper 
 all <-(na.omit(all))
 
 
 dim(all)
 #have 453 total stats for rookies
 
-
-
 #Non-Rookie Stats: After Year 1
 non_rookie <- p %>% filter(year_exp > 1) 
 nr <- non_rookie %>% left_join(draft_wr[c(3,4,6)], by = c('name'= 'pfr_name'))
+#Row 1373 of x
+#row 14 of y
+
+
+#Go over this, fill with 0s???
 nr <- na.omit(nr)
 
 length(unique(nr$name))
 #434 total players have stats from year two and on
+
+
+o <- all[c('pfr_name','epa')]
+
+#Matching to combine
+t <- nr %>% left_join(o, by = c('name' = 'pfr_name'), multiple='first')
+length(unique(t$name))
+
+#SHOULD I ZERO THE NAs because they didnt play
+t <- t[c(2,3,4,10:14)]
+
+names(t)[8] = 'rookie_epa'
+names(t)[3] = 'season_epa'
+
+
+#EPA in rookie season plotted against epa in next season
+library(ggplot2)
+
+#Scatter of the Two against each other
+qplot(x= rookie_epa, y = season_epa, data = t, geom = c("point","smooth"))
+
+#Hist of Season_EPA
+qplot(season_epa, data = t, geom='histogram')
+
+#Fails the test, non-normal
+shapiro.test(t$season_epa)
+
+#There is significant correlation between the two
+cor.test(t$season_epa, t$rookie_epa)
+
+#Pick as well there is correlation --> Higher pick, lower epa
+cor.test(t$season_epa, t$pick)
+
+#Round as well --> Explore wihtin each round
+cor.test(t$season_epa, t$round)
+
+#Season EPA
+t %>% ggplot(aes(x=season_epa, fill = as.factor(round))) + geom_histogram() + labs(fill="")
+
+#Boxplots
+bp_season <- ggplot(t, aes(x = round, y=season_epa, group = round, fill=round)) + geom_boxplot() + coord_cartesian(ylim=c(-60,160))
+bp_rookie <- ggplot(t, aes(x = round, y=rookie_epa, group = round, fill=round)) + geom_boxplot() + coord_cartesian(ylim=c(-60,160))
+
+mean(t$season_epa)
+mean(t$rookie_epa, na.rm = T)
+
+bp_season
+bp_rookie
+
+
+#Round with Rookie and Season
+t %>% group_by(round) %>% summarise(N = length(round), Mean_Season_EPA = mean(season_epa), Variance_Season_EPA = var(season_epa), Mean_Rookie_EPA = mean(rookie_epa, na.rm =T), Variance_Rookie_EPA = var(rookie_epa, na.rm=T)) 
+
+
+#Differences--> These are differences
+t <- t %>% mutate(Diff_EPA = season_epa - rookie_epa)
+t
+
+bp_diff <- ggplot(t, aes(x = round, y= Diff_EPA, group = round, fill=round)) + geom_boxplot() + coord_cartesian(ylim=c(-100,130))
+bp_diff
+
+#With the Z-Scores
+t %>% group_by(round) %>% summarise(N=length(round), Mean_Diff = mean(Diff_EPA, na.rm=T), Var_Diff = var(Diff_EPA, na.rm=T)) %>% mutate(Z = Mean_Diff - 0 / sqrt(Var_Diff/N))
+
+#Significantly different from zero
+t.test(t$Diff_EPA)
+
+
+#Maybe we can use a KS Test to eengineer the rookie data following a gamma distribution
+#gamma_test in goft package
+#or do so with the exponential
+
+
+#Work with the rookie data to get it to follow a gamma dist with specific hyperparameters
+
+#lambda should be adjusted too
+
 
