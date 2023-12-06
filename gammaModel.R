a9514a08d64365942fe5bd226bcda0744c34b71c
SethCorb <89805209+SethCorb@users.noreply.github.com>
2023-12-06 18:06:02 -0500

    Update gammaModel.R

--- gammaModel.R
+++ gammaModel.R
@@ -19,31 +19,57 @@ beta_prior_rate <- 1
 
 # Number of iterations
 n_iterations <- 2000
+burnin <- 100
+
+# Create matrices to store posterior samples
+posterior_samples_alpha <- matrix(NA, nrow = n_iterations, ncol = n_groups)
+posterior_samples_beta <- matrix(NA, nrow = n_iterations, ncol = n_groups)
 
 # Gibbs sampling for gamma distribution with gamma priors
 for (iteration in 1:n_iterations) {
   # Update group shape parameters (alpha)
   for (j in 1:n_groups) {
-    group_indices <- combined_data$round == j
+    group_indices <- combined_data$rounds == j
     alpha[j] <- rgamma(1, shape = alpha_prior_shape + sum(group_indices), rate = alpha_prior_rate + sum(combined_data$epa[group_indices]))
   }
   
   # Update group rate parameters (beta)
   for (j in 1:n_groups) {
-    group_indices <- combined_data$round == j
+    group_indices <- combined_data$rounds == j
     beta[j] <- rgamma(1, shape = beta_prior_shape + sum(group_indices), rate = beta_prior_rate + sum(combined_data$epa[group_indices]))
   }
   
-  # Print the posterior means for alpha and beta (optional)
-  }
+  # Store posterior samples
+  posterior_samples_alpha[iteration, ] <- alpha
+  posterior_samples_beta[iteration, ] <- beta
+}
+posterior_df_alpha = posterior_samples_alpha[-(1:burnin),]
+posterior_df_beta = posterior_samples_beta[-(1:burnin),]
 
-# Calculate posterior means for shape and rate parameters
-posterior_mean_shape <- mean(alpha)
-posterior_mean_rate <- mean(beta)
+# Convert matrices to data frames
+posterior_df_alpha <- data.frame(posterior_df_alpha)
+posterior_df_beta <- data.frame(posterior_df_beta)
 
-posterior_samples <- data.frame(alpha = alpha, beta = beta, group = factor(1:n_groups))
+# Combine data frames for plotting
+posterior_df_combined <- data.frame(
+  Group = rep(1:n_groups, each = n_iterations),
+  Alpha = c(posterior_samples_alpha),
+  Beta = c(posterior_samples_beta)
+)
 
-# Create a box plot
-ggplot(nr, aes(x = factor(round), y = epa)) +
+# Plot box plots using ggplot2
+library(ggplot2)
+
+# Box plot for alpha
+ggplot(posterior_df_combined, aes(x = factor(Group), y = Alpha)) +
   geom_boxplot() +
-  labs(x = "Group", y = "EPA Values", title = "Box Plot of EPA Values by Group")
+  labs(title = "Posterior Distribution of Alpha (Shape Parameter)",
+       x = "Group",
+       y = "Alpha")
+
+# Box plot for beta
+ggplot(posterior_df_combined, aes(x = factor(Group), y = Beta)) +
+  geom_boxplot() +
+  labs(title = "Posterior Distribution of Beta (Rate Parameter)",
+       x = "Group",
+       y = "Beta")
